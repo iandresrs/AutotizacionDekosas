c13c3e62811060bcec1a865d6ab1f2bfe4c9095d
iandresrs <c.andres1489@gmail.com>
2022-08-05 22:03:10 -0500

    Prueba de automatizacion exitosa

--- src/main/java/com/dekosas/Steps/PaginaCancelarCompraStep.java
+++ src/main/java/com/dekosas/Steps/PaginaCancelarCompraStep.java
@@ -3,6 +3,8 @@ package com.dekosas.Steps;
 import com.dekosas.Models.EsperaImplicita;
 import com.dekosas.PageObjects.PaginaCancelarCompra;
 import net.thucydides.core.annotations.Step;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 
 public class PaginaCancelarCompraStep {
     PaginaCancelarCompra paginaCancelarCompra = new PaginaCancelarCompra();
@@ -20,9 +22,13 @@ public class PaginaCancelarCompraStep {
 
     @Step
     public void eliminarProductos(){
-        esperaImplicita.EsperaImplicita(10);
-        paginaCancelarCompra.getDriver().findElement(paginaCancelarCompra.getBtnEliminarArticulo())
-                .click();
+
+        while (paginaCancelarCompra.getDriver().findElement(paginaCancelarCompra.getBtnEliminarArticulo()).isDisplayed()){
+           esperaImplicita.EsperaImplicita(10);
+           paginaCancelarCompra.getDriver().findElement(paginaCancelarCompra.getBtnEliminarArticulo())
+                   .click();
+           
+       }
     }
 
 }
